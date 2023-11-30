a680c7f5306407616b641aaf75f9a77c2d7e794f
My first demo <phvirani45@gmail.com>
2023-11-30 14:28:17 +0530

    remove const port

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-const port = process.env.PORT || 3000;
 
 
 weatherForm.addEventListener('submit', (e) => {
