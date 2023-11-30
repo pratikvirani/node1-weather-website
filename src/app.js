7122bc3b839e7cf54eef9639bd30a1902e9e7798
My first demo <phvirani45@gmail.com>
2023-11-30 14:40:04 +0530

    port line drag

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast= require('./utils/forecast')
-const port = process.env.PORT || 3000;
 
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
